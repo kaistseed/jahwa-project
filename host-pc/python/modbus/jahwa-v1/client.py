4e6ec4fe9e2c818fc778b9b4b0f29841af4779c7
daltamaulana <daltamaulana@kaist.ac.kr>
2023-04-25 12:24:11 +0900

    Add initial code for jahwa-v1 modbus

---
+++ host-pc/python/modbus/jahwa-v1/client.py
@@ -0,0 +1,365 @@
+#####################################################################
+##                         Import Libraries                        ##         
+#####################################################################
+# Built-in libraries
+import os
+import logging
+import asyncio
+from collections import OrderedDict
+
+# Pymodbus libraries
+from pymodbus.constants import Endian
+from pymodbus.client import AsyncModbusTcpClient
+from pymodbus.transaction import ModbusSocketFramer
+from pymodbus.payload import (
+    BinaryPayloadBuilder,
+    BinaryPayloadDecoder,   
+)
+
+#####################################################################
+##                      Define Helper Function                     ##         
+#####################################################################
+# Start the logger
+_logger = logging.getLogger()
+
+# Set endianness dictionary
+ENDIANNESS = {"<": "LITTLE", ">": "BIG"}
+
+# Define function to send payload to server
+async def send_payload(client, endian):
+    """Send custom payload to server.
+
+    Payload builder can be used to simplify the
+    packing of the payload. The payload builder
+    can be used to pack the payload into a
+    register format.
+    
+    Packing/unpacking depends on your CPU's word/byte order. 
+    Modbus messages are always using big endian.
+    BinaryPayloadBuilder will per default use what your CPU uses.
+    The wordorder is applicable only for 32 and 64 bit values.
+    For example, to write a value 0x12345678 to a 32 bit register,
+    the following combinations could be used to write the register:
+    +++++++++++++++++++++++++++++++++++++++++++++++
+    |  Word Order  | Byte order | Word1  | Word2  |
+    +++++++++++++++++++++++++++++++++++++++++++++++
+    |     Big     |     Big     | 0x1234 | 0x5678 |
+    |     Big     |    Little   | 0x3412 | 0x7856 |
+    |    Little   |     Big     | 0x5678 | 0x1234 |
+    |    Little   |    Little   | 0x7856 | 0x3412 |
+    +++++++++++++++++++++++++++++++++++++++++++++++
+    """
+
+    #############################################################
+    ##                   Define Data Builder                   ##         
+    #############################################################
+    # Get word and byte order
+    word_endian, byte_endian = endian
+
+    ######################### Write I2C #########################
+    # Create payload builder
+    write_i2c_builder = BinaryPayloadBuilder(
+        wordorder=word_endian,
+        byteorder=byte_endian,
+    )
+    
+    # Define address and data length
+    addr_len = 10
+    data_len = 10
+
+    # Populate payload with data
+    # Add the slave address
+    write_i2c_builder.add_32bit_uint(0x0)
+    # Add the address length
+    write_i2c_builder.add_32bit_uint(0x0)
+    # Add the data length
+    write_i2c_builder.add_32bit_uint(0x0)
+    # Add the address buffer
+    for i in range(addr_len):
+        write_i2c_builder.add_8bit_uint(0x0)
+    # Add the data buffer
+    for i in range(data_len):
+        write_i2c_builder.add_8bit_uint(0x0)
+
+    ########################## Read I2C #########################
+    # Create payload builder
+    read_i2c_builder = BinaryPayloadBuilder(
+        wordorder=word_endian,
+        byteorder=byte_endian,
+    )
+
+    # Define address and data length
+    addr_len = 10
+    data_len = 10
+
+    # Populate payload with data
+    # Add the slave address
+    read_i2c_builder.add_32bit_uint(0x0)
+    # Add the address length
+    read_i2c_builder.add_32bit_uint(0x0)
+    # Add the data length
+    read_i2c_builder.add_32bit_uint(0x0)
+    # Add the address buffer
+    for i in range(addr_len):
+        read_i2c_builder.add_8bit_uint(0x0)
+    # Add the data buffer
+    for i in range(data_len):
+        read_i2c_builder.add_8bit_uint(0x0)
+
+    ####################### Pattern Load ########################
+    # Create payload builder
+    pattern_load_builder = BinaryPayloadBuilder(
+        wordorder=word_endian,
+        byteorder=byte_endian,
+    )
+
+    # Define builder parameters
+    wr_packet_size = 10
+    rd_packet_size = 10
+    block_count = 10
+    measure_count = 10
+    wr_buffer_size = wr_packet_size * block_count * measure_count
+
+    # Populate payload with data
+    # Add command ID
+    pattern_load_builder.add_32bit_uint(0x0)
+    # Add write slave address
+    pattern_load_builder.add_32bit_uint(0x0)
+    # Add read slave address
+    pattern_load_builder.add_32bit_uint(0x0)
+    # Add write packet size
+    pattern_load_builder.add_32bit_uint(0x0)
+    # Add read packet size
+    pattern_load_builder.add_32bit_uint(0x0)
+    # Add block count
+    pattern_load_builder.add_32bit_uint(0x0)
+    # Add measure count
+    pattern_load_builder.add_32bit_uint(0x0)
+    # Add write interval
+    pattern_load_builder.add_32bit_uint(0x0)
+    # Add measure interval
+    pattern_load_builder.add_32bit_uint(0x0)
+    # Add delay from write to measure interval
+    pattern_load_builder.add_32bit_uint(0x0)
+    # Add write data buffer
+    for i in range(wr_buffer_size):
+        pattern_load_builder.add_8bit_uint(0x0)
+
+    ######################## Pattern Run ########################
+    # Create payload builder
+    pattern_run_builder = BinaryPayloadBuilder(
+        wordorder=word_endian,
+        byteorder=byte_endian,
+    )
+
+    # Populate payload with data
+    # Add command ID
+    pattern_run_builder.add_32bit_uint(0x0)
+
+    ##################### Pattern Get Data ######################
+    # Create payload builder
+    pattern_get_data_send_builder = BinaryPayloadBuilder(
+        wordorder=word_endian,
+        byteorder=byte_endian,
+    )
+
+    # Populate payload with data
+    # Add read buffer size
+    pattern_get_data_send_builder.add_32bit_uint(0x0)
+    # Add LDO Channel 0 current buffer size
+    pattern_get_data_send_builder.add_32bit_uint(0x0)
+    # Add LDO Channel 0 voltage buffer size
+    pattern_get_data_send_builder.add_32bit_uint(0x0)
+    # Add LDO Channel 1 current buffer size
+    pattern_get_data_send_builder.add_32bit_uint(0x0)
+    # Add LDO Channel 1 voltage buffer size
+    pattern_get_data_send_builder.add_32bit_uint(0x0)
+
+    ##################### Measure Position ######################
+    # Create payload builder
+    measure_position_builder = BinaryPayloadBuilder(
+        wordorder=word_endian,
+        byteorder=byte_endian,
+    )
+
+    # Populate payload with data
+    # Add command ID
+    measure_position_builder.add_32bit_uint(0x0)
+
+    ###################### Set LDO Voltage ######################
+    # Create payload builder
+    set_ldo_voltage_builder = BinaryPayloadBuilder(
+        wordorder=word_endian,
+        byteorder=byte_endian,
+    )
+
+    # Populate payload with data
+    # Add LDO channel
+    set_ldo_voltage_builder.add_32bit_uint(0x0)
+
+    # Add LDO voltage
+    set_ldo_voltage_builder.add_64bit_float(0x0)
+
+
+
+
+
+
+
+
+
+    # Create payload for every combination of word and byte order
+    for word_endian, byte_endian in (
+        (Endian.Big, Endian.Big),
+        (Endian.Big, Endian.Little),
+        (Endian.Little, Endian.Big),
+        (Endian.Little, Endian.Little),
+    ):
+        print("-" * 60)
+        print(f"Word Order: {ENDIANNESS[word_endian]}")
+        print(f"Byte Order: {ENDIANNESS[byte_endian]}")
+        print()
+
+        # Create payload builder
+        builder = BinaryPayloadBuilder(
+            wordorder=word_endian,
+            byteorder=byte_endian,
+        )
+
+        # Populate payload with data
+        # Define address length and data length
+        address_length = 10
+        data_length = 10
+        # Add the slave address
+        builder.add_32bit_uint(1)
+        # Add the address length
+        builder.add_32bit_uint(address_length)
+        # Add the data length
+        builder.add_32bit_uint(data_length)
+        # Add the address buffer
+        for i in range(address_length):
+            builder.add_32bit_uint(i+1)
+        # Add the data buffer
+        for i in range(data_length):
+            builder.add_32bit_uint((i+1)*10)
+
+        #############################################################
+        ##                   Write Data to Slave                   ##         
+        #############################################################
+        # Create register payload
+        registers = builder.to_registers()
+        print("Write registers:")
+        print(registers)
+        print()
+
+        # Build payload
+        payload = builder.build()
+
+        # Define address and slave
+        address = 0
+        slave = 1
+
+        # Write payload to server
+        print(f"Write payload to address {address} of slave {slave}")
+        rr = await client.write_registers(address, registers, slave=slave)
+        assert not rr.isError()
+
+        #############################################################
+        ##                   Read Data from Slave                  ##         
+        #############################################################
+        # Define payload count
+        count = len(payload)
+
+        # Read payload from server
+        print(f"Read payload from address {address} of slave {slave}")
+        rr = await client.read_holding_registers(address, count, slave=slave)
+        assert not rr.isError()
+
+        # Print payload
+        print("Read registers:")
+        print(rr.registers)
+        print()
+
+        # Create custom payload decoder
+        decoder = BinaryPayloadDecoder.fromRegisters(
+            rr.registers,
+            wordorder=word_endian,
+            byteorder=byte_endian,
+        )
+
+        # Make sure the word and byte order is consistent with the payload builder
+        assert decoder._wordorder == builder._wordorder
+        assert decoder._byteorder == builder._byteorder
+
+        # Create dictionary to store payload
+        decoded_payload = OrderedDict(
+            [
+                ("slave", decoder.decode_32bit_uint()),
+                ("address_length", decoder.decode_32bit_uint()),
+                ("data_length", decoder.decode_32bit_uint()),
+                ("address", []),
+                ("data", []),
+            ]
+        )
+
+        # Add address buffer
+        for i in range(decoded_payload["address_length"]):
+            decoded_payload["address"].append(decoder.decode_32bit_uint())
+
+        # Add data buffer
+        for i in range(decoded_payload["data_length"]):
+            decoded_payload["data"].append(decoder.decode_32bit_uint())
+
+        # Print payload
+        print("Decoded payload:")
+        for key, value in decoded_payload.items():
+            print(f"{key}: {value}")
+        print()
+
+# Define function to setup client
+def setup_client(host, port):
+    # Create client
+    client = AsyncModbusTcpClient(
+        host=host,
+        port=port,
+        framer=ModbusSocketFramer,
+    )
+
+    # Return client
+    return client
+
+# Define function to start the client
+async def run_async_client(client, modbus_calls=None):
+    # Set logger info
+    _logger.info("### Running Async Client")
+
+    # Connect to server
+    await client.connect()
+    assert client.connected
+
+    # Send payload to server
+    if modbus_calls:
+        await modbus_calls(client)
+
+    # Disconnect from server
+    await client.close()
+
+    # Set logger info
+    _logger.info("### Closing Async Client")
+
+#####################################################################
+##                            Main Function                        ##
+#####################################################################
+if __name__ == "__main__":
+    # Set host and port
+    host = "127.0.0.1"
+    port = 5020
+
+    # Setup client
+    client = setup_client(host, port)
+
+    # Start client
+    asyncio.run(run_async_client(client, modbus_calls=send_payload), debug=True)
+
+
+
---
+++ host-pc/python/modbus/jahwa-v1/library/modbus/iic.py
@@ -0,0 +1,182 @@
+#####################################################################
+##                         Import Libraries                        ##         
+#####################################################################
+# Built-in libraries
+import os
+import logging
+import asyncio
+from collections import OrderedDict
+
+# Pymodbus libraries
+from pymodbus.constants import Endian
+from pymodbus.payload import (
+    BinaryPayloadBuilder,
+    BinaryPayloadDecoder,   
+)
+
+#####################################################################
+##                     Define Modbus IIC Class                     ##         
+#####################################################################
+# Define endianness dictionary
+ENDIANNESS = {"<": "LITTLE", ">": "BIG"}
+
+# Define Modbus IIC class
+class ModbusIIC:
+    def __init__(self, client):
+        # Start the logger
+        self._logger = logging.getLogger()
+        # Set client
+        self.client = client
+
+    ##################################################################
+    ##                    Define Helper Function                    ##         
+    ##################################################################
+    # Define function to decode payload
+    def _DecodePayload(self, payload, payload_name, **kwargs):
+        # Get endianness
+        word_endian = kwargs.get("word_endian", Endian.Big)
+        byte_endian = kwargs.get("byte_endian", Endian.Big)
+
+        # Get payload and payload name
+        payload = kwargs.get("payload", None)
+        payload_name = kwargs.get("payload_name", None)
+        
+        # Initialize decoder
+        decoder = BinaryPayloadDecoder(
+            payload.registers, # pylint: disable=no-member
+            wordorder=word_endian,
+            byteorder=byte_endian
+        )
+
+        # Create dictionary to store decoded payload
+        decoded_payload = OrderedDict()
+
+        # Decode payload based on payload name
+        # Read IIC Status
+        if payload_name == "read_iic_status":
+            decoded_payload["status"] = decoder.decode_16bit_uint()
+        # Read IIC Data
+        elif payload_name == "read_iic_data":
+            for i in range(4):
+                decoded_payload["data"].append(decoder.decode_8bit_uint())
+        # Raise error if payload name is not defined
+        else:
+            raise ValueError("Payload name is not defined")
+
+        # Return decoded payload
+        return decoded_payload
+
+    ##################################################################
+    ##                      Define Data Builder                     ##         
+    ##################################################################
+    def _BuildData(self, *args, **kwargs):
+        # Get endianness
+        word_endian = kwargs.get("word_endian", Endian.Big)
+        byte_endian = kwargs.get("byte_endian", Endian.Big)
+
+        # Get address and data length
+        address_length = kwargs.get("address_length", 1)
+        data_length = kwargs.get("data_length", 1)
+
+        # Initialize builder
+        builder = BinaryPayloadBuilder(
+            byteorder=word_endian, 
+            wordorder=byte_endian
+        )
+
+        # Add data to builder
+        # Add the slave address
+        slave_address = kwargs.get("slave_address", 0x01)
+        builder.add_16bit_uint(slave_address)
+
+        # Add the address length
+        builder.add_16bit_uint(address_length)
+
+        # Add the data length
+        builder.add_16bit_uint(data_length)
+
+        # Add the address buffer
+        for i in range(4): #NOTE: Check whether 4 is array length or element length
+            builder.add_8bit_uint(0)
+
+        # Add the data buffer
+        for i in range(4): #NOTE: Check whether 4 is array length or element length
+            builder.add_8bit_uint(0)
+
+        # Return builder
+        return builder
+
+    ##################################################################
+    ##                  Define Write Method for IIC                 ##         
+    ##################################################################
+    async def Write(self, *args, **kwargs):
+        # Define local variables
+        write_addr = 0
+        slave = 87 #NOTE: 87 is ASCII code for 'W'
+
+        # Build data
+        builder = self._BuildData(*args, **kwargs)
+
+        # Build data to registers
+        registers = builder.to_registers()
+
+        # Write payload to server
+        self._logger.info(f"Write payload to address {write_addr} of slave {slave}")
+        rr = await self.client.write_registers(write_addr, registers, slave=slave)
+        assert not rr.isError()
+
+    ##################################################################
+    ##                   Define Read Method for IIC                 ##         
+    ##################################################################
+    async def Read(self, *args, **kwargs):
+        # Define local variables
+        status_addr = 0
+        send_addr = 1
+        read_addr = 2
+        slave = 82 #NOTE: 82 is ASCII code for 'R'
+        
+        # Build data
+        builder = self._BuildData(*args, **kwargs)
+
+        # Build data to registers
+        send_regs = builder.to_registers()
+
+        # Write payload to server
+        self._logger.info(f"Write Read IIC payload to address {send_addr} of slave {slave}")
+        rr = await self.client.write_registers(send_addr, send_regs, slave=slave)
+        assert not rr.isError()
+
+        # Read payload from server
+        # Initialize read iic status
+        read_iic_status = 0
+
+        # Read iic status until it is not 0
+        while read_iic_status == 0:
+            # Read iic status
+            self._logger.info(f"Read IIC status from address {status_addr} of slave {slave}")
+            rr = await self.client.read_holding_registers(address=status_addr, slave=slave)
+            assert not rr.isError()
+
+            # Decode payload
+            decoded_status = self._DecodePayload(
+                payload=rr,
+                payload_name="read_iic_status"
+            )
+
+            # Get read iic status
+            read_iic_status = decoded_status["status"]
+
+        # Read iic data
+        self._logger.info(f"Read IIC data from address {read_addr} of slave {slave}")
+        rr = await self.client.read_holding_registers(address=read_addr, slave=slave)
+        assert not rr.isError()
+
+        # Decode payload
+        decoded_data = self._DecodePayload(
+            payload=rr,
+            payload_name="read_iic_data"
+        )
+
+        # Return decoded payload
+        return decoded_data
+
